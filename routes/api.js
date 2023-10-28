3a6e5236429ae525115fb6ac276b7a00ac997eaf
Eli <eli.serra173@gmail.com>
2023-10-29 00:34:13 +0200

    Add api routes

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,44 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
-
+const Ut = require('../utils/utils');
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  const formatNumber = (num) => {
+    return (num) ? utils.round(Number(num), 5) : num;
+  }
+
+  const isResult = (data) => {
+    return (
+      data && data.initNum
+      && data.initUnit && data.returnNum
+      && data.returnUnit && data.string)
+  }
+
+  app.get('/api/convert',
+    (req, res) => {
+      const input = (req.query.input) ? req.query.input : '';
+      console.log("input to convert: ", input)
+
+      let result = convertHandler.convertTo(input);
+      if (isResult(result)) {
+        console.log("convert result: ", result)
+        res.json(result);
+        return;
+      }
+      else {
+        result = (!result) ? convertHandler.formatResult({ initNum: formatNumber(num), unit: unit }) : result
+        console.log("convert result error: ", result)
+        res
+          .type('text')
+          .send(result)
+        return;
+      }
+
+
+
+    });
 
 };
